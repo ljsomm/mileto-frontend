2d0d5bda9e3c67c399d1676dbf5f75e6f0a11df0
ljsomm <miscrits.lucas@gmail.com>
2022-03-14 20:00:36 -0300

    task - componente modal

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
+import { useContext } from 'react';
+import ModalContext from '../../contexts/ModalContext';
 
 const Main = ({ title }) => {
+
+    const { dispatch } = useContext(ModalContext);
 
     useEffect(() => {
         document.title = `Mileto - ${title}`;
@@ -9,6 +13,7 @@ const Main = ({ title }) => {
     return(
         <h1>
             Main works!
+            <button onClick={()=>dispatch({type: "OPEN", modal: {  closeButton: true, header: <p>TÍTULO</p>, body: <form><input type="text" /></form>, footer: <button>FECHAR</button> }})}></button>
         </h1>
     );
 }
