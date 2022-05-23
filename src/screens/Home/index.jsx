1889a59dbf47c67d5245c9f9b0532dc4ab97045f
Eeprio <juniordelimaarcurio@outlook.com>
2022-05-23 20:15:08 -0300

    att estetica da pagina inicial e limpeza do código

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import './styles.css'
+import './styles.css';
 
 const Home = ({ title }) => {
 
@@ -16,10 +16,27 @@ const Home = ({ title }) => {
                 <p>- Texto futuramente gerado automaticamente</p>
             </div>
             <div className='home'>
-                <h1>Home exists</h1>
+                <div className="home-content-left">
+                    <div className="home-content-text">
+                        <h1>Quem somos nós?</h1>
+                        <p>Um breve texto apresentando os integrantes com o video ao lado</p>
+                    </div>
+                    <div className="home-content-video"></div>
+                </div>
+                
             </div>
             <div className='home'>
-                <h1>Home exists</h1>
+            <div className="home-content-right">
+
+            <div className="home-content-video"></div>
+
+                    <div className="home-content-text">
+                        <h1>Sobre a Plataforma</h1>
+                        <p>O projeto Mileto surgiu a vontade de um grupo de fazer algo diferente e 
+                        acessível para todos. Com o intuito de estimular as pessoas a se profissionalizarem
+                        indendente de sua raça, gênero, orientação sexual ou religião.</p>
+                    </div>
+                </div>
             </div>
         </>
         
