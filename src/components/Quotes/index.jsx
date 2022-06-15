7851c0edc1ed6da1074359c8953f51fad9c6980b
ljsomm <miscrits.lucas@gmail.com>
2022-06-15 18:40:22 -0300

    fake merge

---
+++ src/components/Quotes/index.jsx
@@ -0,0 +1,28 @@
+const quotes=[
+	{
+    "quote": "A educação tem raízes amargas, mas os seus frutos são doces.",
+    "author": "Aristóteles"
+    }, {
+    "quote": "Para ganhar conhecimento, adicione coisas todos os dias. Para ganhar sabedoria, elimine coisas todos os dias.",
+    "author": "Lao-Tsé"
+    }, {
+    "quote": "A educação exige os maiores cuidados, porque influi sobre toda a vida.",
+    "author": "Séneca"
+    }, {
+    "quote": "Quem olha para fora, sonha. Quem olha para dentro, desperta.",
+    "author": "Carl Jung"
+    }, {
+    "quote": "A educação é a ferramenta mais poderosa que podemos usar para mudar o mundo.",
+    "author": "Nelson Mandela"
+    }, {
+    "quote": "O conhecimento une cada um consigo mesmo e todos com todos.",
+    "author": "josé Saramago"
+	}, {
+	"quote": "Só a educação liberta.",
+	"author": "Epicteto"
+	}, {
+	"quote": "A vida deve ser uma constante educação.",
+	"author": "Gustave Flaubert"
+	},
+]
+export default quotes;
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react';
-import './styles.css'
+import quotes from '../../components/Quotes';
+import { useState } from 'react';
+import './styles.css';
+
 
 const Home = ({ title }) => {
 
+    const[quote]= useState(quotes[Math.floor(Math.random()*quotes.length)]);
 
     useEffect(() => {
         document.title = `Mileto - ${title}`;
@@ -12,14 +16,31 @@ const Home = ({ title }) => {
 
         <>
             <div className='home'>
-                <h1>"O aprendizado é infinito, nunca pare de aprender"</h1>
-                <p>- Texto futuramente gerado automaticamente</p>
+                <h1 onLoad={useState}> "{quote.quote}" </h1>
+                <p>- {quote.author}</p>
             </div>
             <div className='home'>
-                <h1>Home exists</h1>
+                <div className="home-content-left">
+                    <div className="home-content-text">
+                        <h1>Quem somos nós?</h1>
+                        <p>Um breve texto apresentando os integrantes com o video ao lado</p>
+                    </div>
+                    <div className="home-content-video"></div>
+                </div>
+                
             </div>
             <div className='home'>
-                <h1>Home exists</h1>
+            <div className="home-content-right">
+
+            <div className="home-content-video"></div>
+
+                    <div className="home-content-text">
+                        <h1>Sobre a Plataforma</h1>
+                        <p>O projeto Mileto surgiu a vontade de um grupo de fazer algo diferente e 
+                        acessível para todos. Com o intuito de estimular as pessoas a se profissionalizarem
+                        indendente de sua raça, gênero, orientação sexual ou religião.</p>
+                    </div>
+                </div>
             </div>
         </>
         
