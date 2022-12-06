17c010b338949c5577340cd284c2353e28e3d00e
ljsomm <miscrits.lucas@gmail.com>
2022-12-05 21:28:39 -0300

    v1.0.3-beta

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import UserContext from "../../contexts/UserContext";
 import UserUtil from "../../utils/UserUtil";
 import styles from "./styles.module.scss";
+import ThumbNotebook  from "../../assets/images/thumb-notebook.svg";
 
 const Dashboard = ({ title }) => {
 
@@ -16,7 +17,13 @@ const Dashboard = ({ title }) => {
   <>
     <h1>{UserUtil.grettings(state.name)}</h1>
     <div className={styles["flex-container"]}>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <img src={ThumbNotebook}/>
+        <div>
+          <h2>Revolucione</h2>
+          <p>Aprenda criar engajamento em seus cursos</p>
+        </div>
+      </div>
       <div className={styles.item}></div>
     </div>
     <div className={styles["flex-container"]}>
